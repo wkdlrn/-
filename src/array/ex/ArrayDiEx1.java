f13fec7ff4bd8a0a3f48d3b98909c18926c2d02d
wkdlrn <kkjjgg97@gmail.com>
2024-10-11 16:22:05 +0900

    10-11

--- src/array/ex/ArrayDiEx1.java
+++ src/array/ex/ArrayDiEx1.java
@@ -8,15 +8,12 @@ public class ArrayDiEx1 {
         Scanner scanner = new Scanner(System.in);
         int [][] scores = new int [4][3];
         String [] subjects = {"국어", "영어", "수학"};
-        int sum = 0;
-        double average;
 
         for (int i = 0; i < 4; i++) {
             System.out.println((i + 1) + "번 학생의 성적을 입력하세요:");
             for (int j = 0; j < 3; j++) {
                 System.out.print(subjects[j] + " 점수:");
                 scores[i][j] = scanner.nextInt();
-                sum += scores[i][j];
             }
         }
 
@@ -25,8 +22,8 @@ public class ArrayDiEx1 {
             for (int j = 0; j < 3; j++) {
                 total += scores[i][j];
             }
-            average = total / 3.0;
-            System.out.println((i + 1) + "번 학생의 총점: " + sum + ", 평균: " + average );
+            double average = total / 3.0;
+            System.out.println((i + 1) + "번 학생의 총점: " + total + ", 평균: " + average );
         }
 
 
